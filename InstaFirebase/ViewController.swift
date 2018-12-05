59c3156f24b6ddd0b8375b182be3513552c54e32
YouSS <youss.cs@gmail.com>
2018-12-05 13:19:34 +0100

    Initial Commit

---
+++ InstaFirebase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstaFirebase
+//
+//  Created by YouSS on 12/5/18.
+//  Copyright © 2018 YouSS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
